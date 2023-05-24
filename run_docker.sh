ce4d147bf981a72df69c0eea59014a768272297b
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-25 00:56:34 +0700

    container: run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build . -t thienphuoc1990/udacity-microservices-at-scale
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -p "8000:80" thienphuoc1990/udacity-microservices-at-scale
