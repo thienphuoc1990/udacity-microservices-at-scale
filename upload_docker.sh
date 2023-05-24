5499ae99081d6a24fa617ae99912ff9ea0b982e3
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-25 01:22:47 +0700

    update-docker: upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=thienphuoc1990/udacity-microservices-at-scale
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+echo "Login docker..."
+# $DOCKER_PASSWORD will be export to system environment,
+# like if we use circleci, we can create DOCKER_PASSWORD variable
+echo $DOCKER_PASSWORD | docker login -u "thienphuoc1990" --password-stdin  docker.io
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
